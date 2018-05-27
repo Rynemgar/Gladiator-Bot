6a1684ebf5bdc4c330fee6ba9c56c51f074c9180
Jordan Ide <jorrdandied@hotmail.com>
2018-05-27 22:23:13 +0100

    restrict to one room or dev server

--- core/message-handler.js
+++ core/message-handler.js
@@ -2,8 +2,10 @@ const config = require('../config');
 const commands = require('./commands');
 
 module.exports = message => {
-  if (message.content[0] !== config.prefix) return;
-  if (message.author.bot) return;
-  const trigger = message.content.split(' ')[0].slice(1);
-  if (commands[trigger]) commands[trigger].handler(message);
+  if (message.guild.id === config.devServer || message.channel.name === 'colosseum') {
+    if (message.content[ 0 ] !== config.prefix) return;
+    if (message.author.bot) return;
+    const trigger = message.content.split(' ')[ 0 ].slice(1);
+    if (commands[ trigger ]) commands[ trigger ].handler(message);
+  }
 };
