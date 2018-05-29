d0663ad89b3d9a171b9662775d1a845b82504cbb
Rynemgar <32217647+Rynemgar@users.noreply.github.com>
2018-05-29 18:43:06 +0100

    edit levels

--- leveling.js
+++ leveling.js
@@ -2,28 +2,24 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const config = require("./config.json");
 
-let level = JSON.parse(fs.readFileSync("./levels.json", "utf8"));
+let points = JSON.parse(fs.readFileSync("./points.json", "utf8"));
 
 module.exports = function handler(message) {
-  if (!level[message.author.id]) level[message.author.id] = {
-    level: 0,
-    level: 0
-  };
-  let userData = level[message.author.id];
-  userData.level++;
+  let userData = points[message.author.id];
+  userData.points++;
 
-  let currentLevel = Math.floor(0.1 * Math.sqrt(userData.level));
-  if (currentLevel > userData.level) {
+  let currentLevel = Math.floor(0.1 * Math.sqrt(userData.points));
+  if (currentLevel > userData.points) {
     // Level up!
-    userData.level = currentLevel;
-    message.reply(`You"ve leveled up to level **${currentLevel}**! Ain"t that dandy?`);
+    userData.points = currentLevel;
+    message.reply(`You"ve leveled up to points **${currentLevel}**! Ain"t that dandy?`);
   }
 
-  if (message.content.startsWith(prefix + "level")) {
-    message.reply(`You are currently level ${userData.level}, with ${userData.level} level.`);
+  if (message.content.startsWith(prefix.config + "points")) {
+    message.reply(`You are currently level ${userData.level}.`);
   }
-  fs.writeFile("./levels.json", JSON.stringify(level), (err) => {
+  fs.writeFile("./points.json", JSON.stringify(points), (err) => {
     if (err) console.error(err)
   });
 
-});
+};
