c21c341da96c80650abe9144b44fd176c27cda6d
Rynemgar <32217647+Rynemgar@users.noreply.github.com>
2018-05-30 17:28:26 +0100

    Add message on Level Up

--- core/arena/arena.js
+++ core/arena/arena.js
@@ -115,6 +115,7 @@ class Arena {
               \`Level\` = Level + 1
             WHERE \`UserId\` = ${winner.id};
           `;
+          message.channel.send(`${winner.id} you have levelled up! DING!`);
         } else {
           query = `
             UPDATE \`GladiatorBot\`.\`Levels\` 
