6a3250c66a8c1cd1a8a47ab2df2e18ed2b892870
Jordan Ide <jorrdandied@hotmail.com>
2018-05-31 21:00:17 +0100

    oops

--- core/colosseum.js
+++ core/colosseum.js
@@ -7,8 +7,8 @@ class Colosseum {
     } else {
       client.on('ready', () => {
         console.log(`I am ready... Are You?`);
-        this.send(`The doors open to the arena. Who will be the first challenger?`);
         this.getColosseum();
+        this.send(`The doors open to the arena. Who will be the first challenger?`);
       })
     }
   }
