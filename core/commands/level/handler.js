2875eef6d674c9ae7f11bc69764a9d077d4fadab
Rynemgar <32217647+Rynemgar@users.noreply.github.com>
2018-06-01 18:41:54 +0100

    Updated level call to include wins and losses

--- core/commands/level/handler.js
+++ core/commands/level/handler.js
@@ -13,14 +13,18 @@ class LevelCommand {
     message.delete(1000);
     querySql(`INSERT IGNORE INTO Levels (userId) VALUES (${message.author.id})`)
       .then(() => querySql(`
-        SELECT Level 
+        SELECT Level,
+              Wins,
+              Losses 
         FROM Levels 
         WHERE UserID = ${message.author.id}
       `))
 
       .then((results) => {
         const level = results[0] ? results[ 0 ].Level : 0;
-        message.reply(`You are currently Level ${level}`);
+        const wins = results[1] ? results[ 1 ].Wins : 0;
+        const losses = results[2] ? results[ 2 ].Losses : 0;
+        message.reply(`You are currently Level ${level} with ${wins} wins and ${losses} losses!`);
       })
       .catch(console.error);
 
