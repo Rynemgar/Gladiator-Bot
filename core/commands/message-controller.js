2bb02e5a7a6fdee8de77a0889d2835816db38977
Jordan Ide <jorrdandied@hotmail.com>
2018-06-03 15:48:51 +0100

    remove "isInDevServer" check. to better replicate production

--- core/commands/message-controller.js
+++ core/commands/message-controller.js
@@ -4,9 +4,8 @@ class MessageController {
   handleMessage(message) {
     const isGlobalMessage = this.global;
     const isInColosseum = message.channel.name === 'colosseum';
-    const isInDevServer = message.guild.id === config.devServer;
 
-    if (isGlobalMessage || isInColosseum || isInDevServer) {
+    if (isGlobalMessage || isInColosseum) {
       this.handler(message);
     }
   }
