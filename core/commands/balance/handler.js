637a475b16a9d5f14d2f4087d437ab2864ffa321
Rynemgar <32217647+Rynemgar@users.noreply.github.com>
2018-06-12 17:14:35 +0100

    Updated balance request fixing bug.
    Thanks iburnmycd

--- core/commands/balance/handler.js
+++ core/commands/balance/handler.js
@@ -1,36 +1,31 @@
-const MessageController = require('../message-controller');
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var xhr = new XMLHttpRequest();
+const MessageController = require('../message-controller')
+const request = require('request-promise-native')
+
 class BalanceCommand extends MessageController {
-  constructor() {
-    super();
-    this.global = true;
-    this.cooldown = 20000;
-    
+  constructor () {
+    super()
+    this.global = true
+    this.cooldown = 20000
   }
-  
-  handler(message) {
-    message.delete(1000);
-    if (this.lastUsed + this.cooldown > Date.now()) return;
 
-    xhr.open('GET', "http://krruzic.xyz:5000/balance?pid=ca746b821dad1d8458ec0f78880929049cb7db39d1e5381b8392522871d661d7", true);
-    xhr.send();
-    xhr.addEventListener("readystatechange", processRequest, false);
-    function processRequest(e) {
-      if (xhr.readyState == 4 && xhr.status == 200) {
-        var response = JSON.parse(xhr.responseText);
-        var count = response.balance;
-        message.channel.send(`My current prize fund balance is ${count}`);
-        console.log(`Current Tipjar balance is ${response.balance}`);
-    }
- 
-    }
-    
-    
-    message.channel.send(`To contribute to my prize fund, tip me directly!`);
-  this.lastUsed = Date.now();
-}
-  
+  handler (message) {
+    message.delete(1000)
+    if (this.lastUsed + this.cooldown > Date.now()) return
+    this.lastUsed = Date.now()
+
+    request({
+      uri: 'http://krruzic.xyz:5000/balance?pid=ca746b821dad1d8458ec0f78880929049cb7db39d1e5381b8392522871d661d7',
+      method: 'GET',
+      json: true
+    }).then((response) => {
+      var count = response.balance
+      message.channel.send(`To contribute to my prize fund, tip me directly!`)
+      message.channel.send(`My current prize fund balance is ${count}`)
+      console.log(`Current Tipjar balance is ${count}`)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
 }
 
-module.exports = new BalanceCommand();
+module.exports = new BalanceCommand()
