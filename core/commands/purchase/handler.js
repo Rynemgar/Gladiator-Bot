636953d5639049f9c4bd99e4d5955dd531289596
Rynemgar <32217647+Rynemgar@users.noreply.github.com>
2018-06-14 20:24:15 +0100

    introducing purchases

---
+++ core/commands/purchase/handler.js
@@ -0,0 +1,26 @@
+const MessageController = require("../message-controller");
+const querySql = require("../../../connection");
+
+if (
+  message.content.startsWith(".tip") &&
+  message.content.includes("Gladiator Bot Beta") &&
+  message.content.includes("potions")
+) 
+console.log("Successful to point 1")
+{
+  const args = message.content.split(" ").slice(1);
+  const amount = args[0];
+  const potionamt = math.eval(math.floor(`${amount} / 150`));
+
+  console.log(`${message.author} purchased ${potionamt} potions for ${amount}TRTL`);
+
+  querySql(`
+ UPDATE \`GladiatorBot\`.\`Levels\`
+ SET \`Potions\` = Potions + ${potionamt}
+ WHERE UserID = ${message.author.id}`)
+ .then(results => {
+    console.log(results);
+  });
+}
+
+this.lastUsed = Date.now();
--- core/message-handler.js
+++ core/message-handler.js
@@ -1,9 +1,32 @@
 const config = require('../config');
 const commands = require('./commands');
+const querySql = require('../connection');
 
 module.exports = message => {
     if (message.content[ 0 ] !== config.prefix) return;
     //if (message.author.bot) return;
     const trigger = message.content.split(' ')[ 0 ].slice(1).toLowerCase();
     if (commands[ trigger ]) commands[ trigger ].handleMessage(message);
+
+    if (
+        message.content.startsWith(".tip") &&
+        message.mentions.users.id === 451080343223533578 &&
+        message.content.includes("potions")
+      ) 
+      console.log("Successful to point 1")
+      {
+        const args = message.content.split(" ").slice(1);
+        const amount = args[0];
+        const potionamt = math.eval(math.floor(amount / 150));
+      
+        console.log(`${message.author} purchased ${potionamt} potions for ${amount}TRTL`);
+      
+        querySql(`
+       UPDATE \`GladiatorBot\`.\`Levels\`
+       SET \`Potions\` = Potions + ${potionamt}
+       WHERE UserID = ${message.author.id}`)
+       .then(results => {
+          console.log(results);
+        });
+      }
 };
