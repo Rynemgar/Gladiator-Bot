c6c7f22b78bcf2736f09e6ba1718906112a205b1
Jordan Ide <jorrdandied@hotmail.com>
2018-06-14 20:34:54 +0100

    potion shit

--- core/message-handler.js
+++ core/message-handler.js
@@ -3,31 +3,30 @@ const commands = require('./commands');
 const querySql = require('../connection');
 
 module.exports = message => {
-    if (message.content[ 0 ] !== config.prefix) return;
-    //if (message.author.bot) return;
-    const trigger = message.content.split(' ')[ 0 ].slice(1).toLowerCase();
-    if (commands[ trigger ]) commands[ trigger ].handleMessage(message);
+  //if (message.author.bot) return;
+  const trigger = message.content.split(' ')[ 0 ].slice(1).toLowerCase();
+  if (commands[ trigger ]) {
+    return commands[ trigger ].handleMessage(message);
+  }
 
-    if (
-        message.content.startsWith(".tip") &&
-        message.mentions.users.id === 451080343223533578 &&
-        message.content.includes("potions")
-      ) 
-      
-      {
-        console.log("Successful to point 1")
-        const args = message.content.split(" ").slice(1);
-        const amount = args[0];
-        const potionamt = math.eval(math.floor(amount / 150));
-      
+  if (
+    message.content.startsWith(".tip") &&
+    message.mentions.users.get('451080343223533578') &&
+    message.content.includes("potions")
+  ) {
+    const args = message.content.split(" ").slice(1);
+    const amount = args[ 0 ];
+    const potionamt = Math.floor(amount / 150);
+
+    querySql(`
+         UPDATE \`GladiatorBot\`.\`Levels\`
+         SET \`Potions\` = Potions + ${potionamt}
+         WHERE UserID = ${message.author.id}`)
+      .then(results => {
         console.log(`${message.author} purchased ${potionamt} potions for ${amount}TRTL`);
-      
-        querySql(`
-       UPDATE \`GladiatorBot\`.\`Levels\`
-       SET \`Potions\` = Potions + ${potionamt}
-       WHERE UserID = ${message.author.id}`)
-       .then(results => {
-          console.log(results);
-        });
-      }
+      })
+      .catch(e => {
+        console.error(e);
+      });
+  }
 };
