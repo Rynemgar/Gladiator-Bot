40fcdd44eb8e89970971680fe8ec76218f2d9c32
Unknown <rynemgar@gmail.com>
2018-07-03 17:06:52 +0100

    Stats attempt inside Gladiator.js

--- core/arena/gladiator.js
+++ core/arena/gladiator.js
@@ -1,3 +1,19 @@
+const querySql = require('../../connection')
+
+querySql(`SELECT
+            Strength,
+            Agility,
+            Defense
+          FROM Levels
+          WHERE UserID = ${this.userObject}
+        `)
+        .then((results) => {
+          const strength = results[0] ? results[ 0 ].Strength : 0;
+          const agility = results[0] ? results[ 0 ].Agility : 0;
+          const defense = results[0] ? results[ 0 ].Defense : 0;
+        })
+        .catch(console.error);
+
 class Gladiator {
   constructor(user, baseStats) {
     this.id = user.id;
