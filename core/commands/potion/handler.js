359f293be87291976384c55a404e00d5e1ea3720
Unknown <rynemgar@gmail.com>
2018-07-05 16:14:05 +0100

    Corrected potions command

--- core/commands/potion/handler.js
+++ core/commands/potion/handler.js
@@ -39,7 +39,7 @@ class PotionCommand extends MessageController {
       switch (result.message) {
         case 'HIT':
           //Handle hit
-          message.channel.send(`${result.gladiator.userObject} drinks a potion of obscure colour! Their health seems to improve! ${bold}${result.target.health}hp${bold} remaining!`);
+          message.channel.send(`${result.gladiator.userObject} drinks a potion of obscure colour! Their health seems to improve! ${bold}${result.gladiator.health}hp${bold} remaining!`);
           break;
         case 'NOT_GLADIATOR':
           // Handle not gladiator
